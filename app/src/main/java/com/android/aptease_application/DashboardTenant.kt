aa9118bc8974893d36fc7750ef609049fdcaf374
kamill <camilleannpepito14@gmail.com>
2025-04-25 00:35:08 +0800

    allpagesintprogaptease

---
+++ app/src/main/java/com/android/aptease_application/DashboardTenant.kt
@@ -0,0 +1,15 @@
+package com.android.aptease_application
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DashboardTenant : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard_tenant)
+    }
+}
---
+++ app/src/main/java/com/android/aptease_application/NotificationLandlord.kt
@@ -0,0 +1,8 @@
+package com.android.aptease_application
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
